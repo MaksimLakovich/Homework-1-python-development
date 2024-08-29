0a39dd71e4f1bcd042513a4707cef9f6b20adc52
Maksim Lakovich <maks.lakovich@gmail.com>
2024-08-29 15:30:59 +0300

    Обновлена функция get_date в модуле WIDGET.PY после запуска линтеров

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,5 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
 from datetime import datetime
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(data: str) -> str | None:
@@ -12,11 +11,11 @@ def mask_account_card(data: str) -> str | None:
     number = blocks[-1]
 
     if len(number) == 16:
-        mask_number = get_mask_card_number(number)
+        mask_number = get_mask_card_number(int(number))
         type_and_mask_number = f"{blocks[0]} {mask_number}"
         return type_and_mask_number
     elif len(number) == 20:
-        mask_number = get_mask_account(number)
+        mask_number = get_mask_account(int(number))
         type_and_mask_number = f"{blocks[0]} {mask_number}"
         return type_and_mask_number
     else:
