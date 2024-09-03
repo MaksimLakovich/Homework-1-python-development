9fed9d81d9b6fa5c1cfbdf3086d0fe3375723d9c
Maksim Lakovich <maks.lakovich@gmail.com>
2024-09-03 18:26:19 +0300

    Создал функцию sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,10 @@ def filter_by_state(list_transactions: list, transaction_state="EXECUTED") -> li
     у которых ключ 'state' соответствует указанному значению"""
 
     return [transaction for transaction in list_transactions if transaction["state"] == transaction_state]
+
+
+def sort_by_date(list_transactions: list, sort_parameter=True) -> list:
+    """Функция возвращает новый список словарей, отсортированный по дате (date) с помощью
+    необязательного параметра, задающего порядок сортировки (по умолчанию — убывание)"""
+
+    return [sorted(list_transactions, key=lambda transaction: transaction["date"], reverse=sort_parameter)]
