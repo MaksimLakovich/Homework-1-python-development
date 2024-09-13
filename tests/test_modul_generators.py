a8a51dc0002fedb60e625b3ba30ecab3cc61f1e4
Maksim Lakovich <maks.lakovich@gmail.com>
2024-09-13 18:36:59 +0300

    Добавил тесты для генератора transaction_descriptions. Выполнил проверку кода flake8, black, isort, mypy. Проверил покрытие кода тестами

--- tests/test_modul_generators.py
+++ tests/test_modul_generators.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions
 
 
 @pytest.mark.parametrize(
@@ -33,3 +33,15 @@ def test_with_not_exist_money(full_transactions_list: list[dict[str, Any]]) -> N
     result = filter_by_currency(full_transactions_list, money="UAH")
     with pytest.raises(StopIteration):
         next(result)
+
+
+def test_positive_transaction_descriptions(full_transactions_list: list[dict[str, Any]]) -> None:
+    """Тестируем позитивные случаи поочередного получения descriptions каждой транзакции"""
+
+    description = transaction_descriptions(full_transactions_list)
+
+    assert next(description) == "Перевод организации"
+    assert next(description) == "Перевод со счета на счет"
+    assert next(description) == "Перевод со счета на счет"
+    assert next(description) == "Перевод с карты на карту"
+    assert next(description) == "Перевод организации"
