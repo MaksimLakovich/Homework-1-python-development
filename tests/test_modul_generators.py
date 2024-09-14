109069264e76be647a425348a1ac3876caf02ed0
Maksim Lakovich <maks.lakovich@gmail.com>
2024-09-14 14:42:13 +0300

    Добавил тесты для генератора card_number_generator. Выполнил проверку кода flake8, black, isort, mypy. Проверил покрытие кода тестами

--- tests/test_modul_generators.py
+++ tests/test_modul_generators.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 @pytest.mark.parametrize(
@@ -45,3 +45,31 @@ def test_positive_transaction_descriptions(full_transactions_list: list[dict[str
     assert next(description) == "Перевод со счета на счет"
     assert next(description) == "Перевод с карты на карту"
     assert next(description) == "Перевод организации"
+
+
+@pytest.mark.parametrize(
+    "start, finish, expected",
+    [
+        (3493341234, 3493341234, "0000 0034 9334 1234"),
+        (35, 35, "0000 0000 0000 0035"),
+        (0, 0, "0000 0000 0000 0000"),  # граничное значение
+        (9999999999999999, 9999999999999999, "9999 9999 9999 9999"),  # граничное значение
+    ],
+)
+def test_positive_card_number_generator(start: int, finish: int, expected: str) -> None:
+    """Тестируем выдачу правильных номеров карт в заданном диапазоне + граничные значения"""
+
+    card_num = card_number_generator(start, finish)
+
+    assert next(card_num) == expected
+
+
+def test_correct_range_card_number_generator(expected_card_number_for_generator: list[dict[str, Any]]) -> None:
+    """Тестируем корректную генерацию номеров карт в заданном диапазоне"""
+
+    card_num = card_number_generator(3493341234, 3493341243)
+
+    # Прохожу по списку ожидаемых значений:
+    for expected_card_number in range(len(expected_card_number_for_generator)):
+        # Сравниваю каждый элемент из генератора с соответствующим значением фикстуры:
+        assert next(card_num) == expected_card_number_for_generator[expected_card_number]
