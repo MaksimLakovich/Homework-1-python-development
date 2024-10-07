c5daec4f33e958e6251d130c7f67670e91284adc
Maksim Lakovich <maks.lakovich@gmail.com>
2024-10-07 15:09:58 +0300

    Доработал функцию filter_by_state() в модуле processing чтоб она могла обрабатывать пустые строки

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,15 @@
-def filter_by_state(list_transactions: list, transaction_state="EXECUTED") -> list:
+def filter_by_state(list_transactions: list, transaction_state: str = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ 'state' соответствует указанному значению"""
 
-    return [transaction for transaction in list_transactions if transaction["state"] == transaction_state]
+    return [
+        transaction
+        for transaction in list_transactions
+        if (state := transaction.get("state")) and isinstance(state, str) and transaction["state"] == transaction_state
+    ]
 
 
-def sort_by_date(list_transactions: list, sort_parameter=True) -> list:
+def sort_by_date(list_transactions: list, sort_parameter: bool = True) -> list:
     """Функция возвращает новый список словарей, отсортированный по дате (date) с помощью
     необязательного параметра, задающего порядок сортировки (по умолчанию — убывание)"""
 
